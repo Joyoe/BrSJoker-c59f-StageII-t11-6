ec4bf07155c99e566496c6957ec044d46e57ddc8
KNAI <nair@brightsight.com>
2022-02-16 18:00:33 +0100

    first commit

---
+++ stub/build/generated/source/buildConfig/release/com/brightsight/joker/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.brightsight.joker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.brightsight.joker";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "307ec272";
+  // Field from default config.
+  public static final String APK_URL = null;
+  // Field from default config.
+  public static final int STUB_VERSION = 21;
+}
